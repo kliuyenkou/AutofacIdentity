cfbcb795343eff3f0e3e57562a2a4fd7d1a1b88b
kliuyenkou <klyuenkov@gmail.com>
2016-12-06 19:30:44 +0300

    Register dependencies.

--- AutofacIdentity/Startup.cs
+++ AutofacIdentity/Startup.cs
@@ -1,4 +1,13 @@
-﻿using Microsoft.Owin;
+﻿using System.Web;
+using System.Web.Mvc;
+using Autofac;
+using Autofac.Integration.Mvc;
+using AutofacIdentity.Identity;
+using AutofacIdentity.Models;
+using Microsoft.AspNet.Identity;
+using Microsoft.Owin;
+using Microsoft.Owin.Security;
+using Microsoft.Owin.Security.DataProtection;
 using Owin;
 
 [assembly: OwinStartupAttribute(typeof(AutofacIdentity.Startup))]
@@ -8,7 +17,34 @@ namespace AutofacIdentity
     {
         public void Configuration(IAppBuilder app)
         {
+            ConfigureAutofac(app);
             ConfigureAuth(app);
+        }
+
+        public static void ConfigureAutofac(IAppBuilder app)
+        {
+            var builder = new ContainerBuilder();
+
+            // REGISTER DEPENDENCIES
+            builder.RegisterType<ApplicationDbContext>().AsSelf().InstancePerRequest();
+            builder.RegisterType<ApplicationUserStore>().As<IUserStore<ApplicationUser>>().InstancePerRequest();
+            builder.RegisterType<ApplicationUserManager>().AsSelf().InstancePerRequest();
+            builder.RegisterType<ApplicationSignInManager>().AsSelf().InstancePerRequest();
+            builder.Register<IAuthenticationManager>(c => HttpContext.Current.GetOwinContext().Authentication).InstancePerRequest();
+            builder.Register<IDataProtectionProvider>(c => app.GetDataProtectionProvider()).InstancePerRequest();
+
+            // REGISTER CONTROLLERS SO DEPENDENCIES ARE CONSTRUCTOR INJECTED
+            builder.RegisterControllers(typeof(MvcApplication).Assembly);
+
+            // BUILD THE CONTAINER
+            var container = builder.Build();
+
+            // REPLACE THE MVC DEPENDENCY RESOLVER WITH AUTOFAC
+            DependencyResolver.SetResolver(new AutofacDependencyResolver(container));
+
+            // REGISTER WITH OWIN
+            app.UseAutofacMiddleware(container);
+            app.UseAutofacMvc();
         }
     }
 }
